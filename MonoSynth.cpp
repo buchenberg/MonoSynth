24a6866fb935b6f7a54a7b9d1cac615847c90910
Gregory Buchenberger <buchenberger@gmail.com>
2022-02-22 20:09:56 -0700

    better envelope behavior

--- MonoSynth.cpp
+++ MonoSynth.cpp
@@ -13,9 +13,7 @@ using namespace daisysp;
 static DaisyPod pod;
 static Oscillator osc;
 static MoogLadder flt;
-static AdEnv ad;
 static Adsr adsr;
-static Metro      tick;
 static Parameter cutoffParam, resParam;
 static ReverbSc verb;
 
@@ -23,7 +21,9 @@ bool gate;
 int wave, mode;
 float oscFreq, attack, release, cutoff, fltRes;
 float oldk1, oldk2, k1, k2;
+float sustain;
 bool selfCycle;
+float last_note;
 
 void ConditionalParameter(float oldVal,
                           float newVal,
@@ -36,9 +36,7 @@ void NextSamples(float &sig)
 {
     float adsr_out = adsr.Process(gate);
     osc.SetAmp(adsr_out);
-    //float ad_out = ad.Process();
     sig = osc.Process();
-    //sig *= ad_out;
     sig = flt.Process(sig);
 }
 
@@ -57,35 +55,44 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 
         // Add reverb
         verb.Process(sig, sig, &out[LEFT], &out[RIGHT]);
-
     }
 }
 
+bool CompareFloat(float x, float y, float epsilon = 0.01f)
+{
+    if (fabs(x - y) < epsilon)
+        return true; // they are same
+    return false;    // they are not same
+}
 // Typical Switch case for Message Type.
 void HandleMidiMessage(MidiEvent midiEvent)
 {
+
     switch (midiEvent.type)
     {
     case NoteOn:
     {
-        gate = true;
         NoteOnEvent noteOnEvent = midiEvent.AsNoteOn();
+        gate = true;
+        last_note = (float)noteOnEvent.note;
         osc.SetFreq(mtof(noteOnEvent.note));
+        adsr.SetSustainLevel((noteOnEvent.velocity / 127.0f));
         osc.SetAmp((noteOnEvent.velocity / 127.0f));
-        // ad.Trigger();
         break;
     }
     case NoteOff:
     {
-        gate = false;
-        // NoteOffEvent notOffEvent = midiEvent.AsNoteOff();
+        NoteOffEvent noteOffEvent = midiEvent.AsNoteOff();
+        if (CompareFloat((float)noteOffEvent.note, last_note))
+        {
+            gate = false;
+        }
         break;
     }
     default:
         break;
     }
 }
-
 // Main -- Init, and Midi Handling
 int main(void)
 {
@@ -93,19 +100,20 @@ int main(void)
     float samplerate;
     oldk1 = oldk2 = 0;
     k1 = k2 = 0;
-    attack    = .01f;
-    release   = .2f;
-    cutoff    = 10000;
+    attack = .01f;
+    release = .2f;
+    cutoff = 10000;
     mode = 0;
+    sustain = .25;
+    last_note = 0;
 
-    pod.Init();
+    pod.Init(true);
     pod.SetAudioBlockSize(4);
     samplerate = pod.AudioSampleRate();
     pod.seed.usb_handle.Init(UsbHandle::FS_INTERNAL);
     System::Delay(250);
     adsr.Init(samplerate);
     osc.Init(samplerate);
-    ad.Init(samplerate);
     flt.Init(samplerate);
 
     // Synthesis
@@ -116,18 +124,11 @@ int main(void)
     osc.SetWaveform(wave);
     osc.SetAmp(0.25);
 
-    //Set ADSR envelope parameters
+    // Set ADSR envelope parameters
     adsr.SetTime(ADSR_SEG_ATTACK, .1);
     adsr.SetTime(ADSR_SEG_DECAY, .1);
     adsr.SetTime(ADSR_SEG_RELEASE, .01);
-    adsr.SetSustainLevel(.25);
-
-    // Set AD envelope parameters
-    ad.SetTime(ADENV_SEG_ATTACK, 0.1);
-    ad.SetTime(ADENV_SEG_DECAY, 0.1);
-    ad.SetMax(1);
-    ad.SetMin(0);
-    ad.SetCurve(0.5);
+    adsr.SetSustainLevel(sustain);
 
     // Ladder filter parameters
     flt.SetFreq(10000);
@@ -148,8 +149,9 @@ int main(void)
     pod.midi.StartReceive();
 
     // Execute
-    while(1) {
-         pod.midi.Listen();
+    while (1)
+    {
+        pod.midi.Listen();
         // Handle MIDI Events
         while (pod.midi.HasEvents())
         {
@@ -178,15 +180,16 @@ void UpdateEncoder()
     osc.SetWaveform(wave);
 
     mode += pod.encoder.Increment();
-    
-    if (mode > 1){
+
+    if (mode > 1)
+    {
         mode = 1;
     }
 
-    if (mode < 0){
+    if (mode < 0)
+    {
         mode = 0;
     }
-
 }
 
 void UpdateKnobs()
@@ -194,21 +197,22 @@ void UpdateKnobs()
     k1 = pod.knob1.Process();
     k2 = pod.knob2.Process();
 
-    switch(mode)
+    switch (mode)
     {
-        case 0:
-            ConditionalParameter(oldk1, k1, cutoff, cutoffParam.Process());
-            ConditionalParameter(oldk2, k2, fltRes, resParam.Process());
-            flt.SetFreq(cutoff);
-            flt.SetRes(fltRes);
-            break;
-        case 1:
-            ConditionalParameter(oldk1, k1, attack, pod.knob1.Process());
-            ConditionalParameter(oldk2, k2, release, pod.knob2.Process());
-            ad.SetTime(ADENV_SEG_ATTACK, attack);
-            ad.SetTime(ADENV_SEG_DECAY, release);
-            break;
-        default: break;
+    case 0:
+        ConditionalParameter(oldk1, k1, cutoff, cutoffParam.Process());
+        ConditionalParameter(oldk2, k2, fltRes, resParam.Process());
+        flt.SetFreq(cutoff);
+        flt.SetRes(fltRes);
+        break;
+    case 1:
+        ConditionalParameter(oldk1, k1, attack, pod.knob1.Process());
+        ConditionalParameter(oldk2, k2, release, pod.knob2.Process());
+        adsr.SetTime(ADSR_SEG_ATTACK, attack);
+        adsr.SetTime(ADSR_SEG_RELEASE, release);
+        break;
+    default:
+        break;
     }
 }
 
@@ -216,25 +220,26 @@ void UpdateLeds()
 {
     pod.led1.Set(mode == 2, mode == 1, mode == 0);
 
-    switch(wave){
-        case osc.WAVE_SIN:
-            pod.led2.Set(0, 0, 1);//blue
-            break;
-        case osc.WAVE_TRI:
-            pod.led2.Set(1, 1, 0);//yellow
-            break;
-        case osc.WAVE_SAW:
-            pod.led2.Set(1, 0, 1);//pink
-            break;
-        case osc.WAVE_RAMP:
-            pod.led2.Set(1, 0, 0);//red
-            break;
-        case osc.WAVE_SQUARE:
-            pod.led2.Set(0, 1, 0);//green
-            break;
-        default:
-            pod.led2.Set(1, 0, 1);
-            break;
+    switch (wave)
+    {
+    case osc.WAVE_SIN:
+        pod.led2.Set(0, 0, 1); // blue
+        break;
+    case osc.WAVE_TRI:
+        pod.led2.Set(1, 1, 0); // yellow
+        break;
+    case osc.WAVE_SAW:
+        pod.led2.Set(1, 0, 1); // pink
+        break;
+    case osc.WAVE_RAMP:
+        pod.led2.Set(1, 0, 0); // red
+        break;
+    case osc.WAVE_SQUARE:
+        pod.led2.Set(0, 1, 0); // green
+        break;
+    default:
+        pod.led2.Set(1, 0, 1);
+        break;
     }
 
     oldk1 = k1;
@@ -242,7 +247,6 @@ void UpdateLeds()
 
     pod.UpdateLeds();
 }
-
 
 void Controls()
 {
@@ -254,5 +258,4 @@ void Controls()
     UpdateKnobs();
 
     UpdateLeds();
-
 }
